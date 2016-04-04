8f5f3c52366f488025b00b6476a701ff22ce61e2
eivindw <eivindw@gmail.com>
2016-04-04 22:04:25 +0200

    Prepare to release first version.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject clj-date-no "0.1.0-SNAPSHOT"
+(defproject clj-date-no "0.1.0"
   :description "Library calculating holidays and workdays for Norway."
   :url "https://github.com/eivindw/clj-date-no"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [easter-day "0.1.1"]])
+                 [easter-day "0.1.1"]]
+  :deploy-repositories [["releases" :clojars]])

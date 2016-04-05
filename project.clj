84f3a62690bbc28c29667caaf165304b25bae1c9
eivindw <eivindw@gmail.com>
2016-04-05 20:02:32 +0200

    Add snapshots default to clojars.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [easter-day "0.1.1"]]
-  :deploy-repositories [["releases" :clojars]])
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]])
